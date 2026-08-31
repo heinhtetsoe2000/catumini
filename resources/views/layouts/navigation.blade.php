7848646d7f9508febd1c7a58eff332b5a34dbe22
Hein Htet Soe <soe991398@gmail.com>
2026-08-31 13:53:23 +0630

    feat(login): Improve Login UI (#26)

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,19 +1,5 @@
-<nav class="sticky top-0 z-50 border-b border-black/10 bg-white dark:border-white/10 dark:bg-black">
-    <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-        <div class="flex h-16 items-center justify-between gap-3">
-            <flux:brand :href="route('home')" logo="logo-with-bg.png" wire:navigate/>
+<div>
+    <x-nav-bar.desktop />
 
-            <div class="flex min-w-0 items-center gap-4 sm:gap-8">
-                <x-nav-bar.desktop class="hidden sm:flex gap-4" />
-
-                <x-nav-bar.mobile class="flex sm:hidden gap-4" />
-            </div>
-
-            <div class="flex items-center">
-                <x-nav-profile.desktop class="hidden sm:block" />
-
-                <x-nav-profile.mobile class="flex justify-center sm:hidden" />
-            </div>
-        </div>
-    </div>
-</nav>
+    <x-nav-bar.mobile />
+</div>
--- routes/auth.php
+++ routes/auth.php
@@ -11,8 +11,7 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-    Route::get('login', [AuthenticatedSessionController::class, 'create'])
-        ->name('login');
+    Route::livewire('login', 'pages::auth.login')->name('login');
 
     Route::post('login', [AuthenticatedSessionController::class, 'store']);
 
